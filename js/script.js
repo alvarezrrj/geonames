df8a955bca1a4e6fa920c89f2677e9bc60c02b49
Rodrigo Alvarez <65652568+alvarezrrj@users.noreply.github.com>
2021-01-21 16:03:52 +0000

    v1.0 - all APIs working

--- js/script.js
+++ js/script.js
@@ -8,17 +8,28 @@ $(window).on("load", function() {
 
 //I'm using the fetch API instead of $.ajax because I'm more familiar with it
 $('#searchButton').click(function() { 
+	let endpoint = $('#endpoint').val();
+
    	fetch(`php/blabla.php?lat=${$('#lat').val()}&long=${$('#long').val()}&endpoint=${$('#endpoint').val()}`)
 			.then(response => response.json())
 			.then(function(response){
-					if (response.data.ocean){
+					console.log(response);
+					if (endpoint == "oceanJSON" && response.data.ocean){
 							$('#results').html(`Ocean name: ${response.data.ocean.name}.`);
+					}
+					else if (endpoint == "gtopo30JSON" && response.data.gtopo30){
+							let elev = response.data.gtopo30;
+							$('#results').html(
+									elev == -9999 ? 'Invalid coordinates' : `Elevation: ${elev}m`
+							)
+					}
+					else if (endpoint == "findNearbyPlaceNameJSON" && response.data.geonames[0]){
+							$('#results').html(response.data.geonames[0].adminName1);
 					}
 					else {
 							$('#results').html(`Invalid coordinates`);
 					}
 
 			})
-			.then(response => console.log(response.data))
 			.catch(error => console.error(error));
 });	

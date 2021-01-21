95b00290f60dd2be04ed39974c6167c3bf0b0ee7
Rodrigo Alvarez <65652568+alvarezrrj@users.noreply.github.com>
2021-01-21 12:30:32 +0000

    Ocean up and running

--- js/script.js
+++ js/script.js
@@ -8,20 +8,17 @@ $(window).on("load", function() {
 
 //I'm using the fetch API instead of $.ajax because I'm more familiar with it
 $('#searchButton').click(function() { 
-		fetch(`php/blabla.php`, {
-			method: "POST",
-			headers: {
-					'Content-Type': 'application/json',
-					'lat': $('#lat').val(),
-					'long': $('#long').val(),
-					'endpoint': $('#endpoint').val()
-			}/*,
-			body: JSON.stringify({
-				'lat': $('#lat').val(),
-				'long': $('#long').val(),
-				'endpoint': $('#endpoint').val()
-			})*/
-	})
-	.then(data => $('#results').html(data))
-	.catch(error => console.error(error));
+   	fetch(`php/blabla.php?lat=${$('#lat').val()}&long=${$('#long').val()}&endpoint=${$('#endpoint').val()}`)
+			.then(response => response.json())
+			.then(function(response){
+					if (response.data.ocean){
+							$('#results').html(`Ocean name: ${response.data.ocean.name}.`);
+					}
+					else {
+							$('#results').html(`Invalid coordinates`);
+					}
+
+			})
+			.then(response => console.log(response.data))
+			.catch(error => console.error(error));
 });	
--- php/blabla.php
+++ php/blabla.php
@@ -3,19 +3,11 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-$lat = $_POST['lat'];
-$long = $_POST['long'];
-$endpoint = $_POST['endpoint'];
-
-echo "\n";
-echo print_r($_POST);
-echo "\n";
-echo print_r($_REQUEST);
-echo "\n";
-echo print_r($_GET);
+$lat = $_REQUEST['lat'];
+$long = $_REQUEST['long'];
+$endpoint = $_REQUEST['endpoint'];
 
 $url = 'http://api.geonames.org/' . $endpoint . '?username=alvarezrrj' . '&lat=' . $lat . '&lng=' . $long;
-
 
 $ch = curl_init();
 
@@ -25,27 +17,16 @@ curl_setopt($ch, CURLOPT_URL, $url);
 
 $result = curl_exec($ch);
 
-echo "\n";
-echo $result;
-echo "\n";
-
 curl_close($ch);
 
 $data = json_decode($result, true);
 
-
 $response['status']['code'] = '200';
 $response['status']['name'] = 'ok';
-//$response['status']['description'] = 'all good';
-//$response['status']['returnedIn'] = "x ms";
 $response['data'] = $data;
 
 header('Content-Type: application/json; charset=UTF-8');
 
-echo "\n";
 echo json_encode($response);
-echo "\n";
-echo json_encode($url);
-echo "\n";
 
 ?>
